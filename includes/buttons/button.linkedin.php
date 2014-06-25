8d8eb5cdaa9de8c3dac3bd7a0396d0da46acc9fd
Devon Hillard <devon@sparkred.com>
2014-06-25 10:53:20 -0400

    Changing LinkedIn JavaScript references to HTTPS.

--- includes/buttons/button.linkedin.php
+++ includes/buttons/button.linkedin.php
@@ -8,23 +8,23 @@ class MR_Social_Sharing_Toolkit_Linkedin extends MR_Social_Sharing_Toolkit_Butto
 		$this->title = 'LinkedIn';
 		$this->icon = 'linkedin';
 	}
-	
+
 	function li_share($url, $title, $type, $id, $media = '', $description = '', $text = '', $icon = '') {
 		switch ($type) {
 			case 'horizontal':
 				$retval = '<script type="IN/Share" data-url="'.$url.'" data-counter="right"></script>';
 				$footer = (get_option('mr_social_sharing_js_footer') == 1) ? true : false;
-				$this->enqueue_script('Social_sharing_linkedin', 'http://platform.linkedin.com/in.js', $footer);
+				$this->enqueue_script('Social_sharing_linkedin', 'https://platform.linkedin.com/in.js', $footer);
 				break;
 			case 'vertical':
 				$retval = '<script type="IN/Share" data-url="'.$url.'" data-counter="top"></script>';
 				$footer = (get_option('mr_social_sharing_js_footer') == 1) ? true : false;
-				$this->enqueue_script('Social_sharing_linkedin', 'http://platform.linkedin.com/in.js', $footer);
+				$this->enqueue_script('Social_sharing_linkedin', 'https://platform.linkedin.com/in.js', $footer);
 				break;
 			case 'none':
 				$retval = '<script type="IN/Share" data-url="'.$url.'"></script>';
 				$footer = (get_option('mr_social_sharing_js_footer') == 1) ? true : false;
-				$this->enqueue_script('Social_sharing_linkedin', 'http://platform.linkedin.com/in.js', $footer);
+				$this->enqueue_script('Social_sharing_linkedin', 'https://platform.linkedin.com/in.js', $footer);
 				break;
 			default:
 				$url = 'http://www.linkedin.com/shareArticle?mini=true&amp;url='.urlencode($url).'&amp;title='.urlencode($title);
@@ -34,21 +34,21 @@ class MR_Social_Sharing_Toolkit_Linkedin extends MR_Social_Sharing_Toolkit_Butto
 		}
 		return $retval;
 	}
-	
+
 	function follow_linked($type, $id, $text = '', $icon = '') {
 		$url = 'http://www.linkedin.com/in/'.$id;
 		$text = ($text == '') ? __('Join my network on','mr_social_sharing_toolkit').' LinkedIn' : $text;
 		$blank = (get_option('mr_social_sharing_follow_new') == 1) ? true : false;
 		return $this->get_icon($type, $url, $text, $icon, false, $blank);
 	}
-	
+
 	function follow_linked_co($type, $id, $text = '', $icon = '') {
 		$url = 'http://www.linkedin.com/company/'.$id;
 		$text = ($text == '') ? __('Follow my company on','mr_social_sharing_toolkit').' LinkedIn' : $text;
 		$blank = (get_option('mr_social_sharing_follow_new') == 1) ? true : false;
 		return $this->get_icon($type, $url, $text, $icon, false, $blank);
 	}
-	
+
 	function follow_linked_group($type, $id, $text = '', $icon = '') {
 		$url = 'http://www.linkedin.com/groups?gid='.$id;
 		$text = ($text == '') ? __('Join my group on','mr_social_sharing_toolkit').' LinkedIn' : $text;
